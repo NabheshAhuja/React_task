f5a1ef4f43c06a1bb5731b4792749df38ecfe971
NabheshAhuja <nabheshahuja@gmail.com>
2022-12-31 16:57:42 +0530

    Some changes

--- src/Components/SecondDepthButtons.jsx
+++ src/Components/SecondDepthButtons.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
 
-const SecondDepthButtons = (counter, { JSONData }) => {
+const SecondDepthButtons = ({counter,  JSONData }) => {
   console.log(counter);
   console.log('function executed');
 
   // setShowDiv(!showDiv);
   const firstDepthValue = Object.values(JSONData);
   const secondDepthKeys = Object.keys(firstDepthValue[counter]);
-  console.log(secondDepthKeys);
+  // console.log(secondDepthKeys);
 
-  return secondDepthKeys.map((button, key) => {
-    return (
-      <div
-        className='w3-sidebar w3-bar-block w3-black w3-card'
-        style={{ width: '130px' }}>
-        <span className='w3-bar-item w3-button tablink' key={key}>
-          {button}
-        </span>
-      </div>
-    );
-  });
+  // return secondDepthKeys.map((button, key) => {
+  //   return (
+  //     <div
+  //       key={key}
+  //       className='w3-sidebar w3-bar-block w3-black w3-card'
+  //       style={{ width: '130px' }}>
+  //       <span className='w3-bar-item w3-button tablink' >
+  //         {button}
+  //       </span>
+  //     </div>
+  //   );
+  // });
 };
 
 export default SecondDepthButtons;
--- src/Components/Sharedtextarea.js
+++ src/Components/Sharedtextarea.js
@@ -6,30 +6,72 @@ const JSONData = require('./aekr1SPc.json');
 const Sharedtextarea = () => {
   const firstDepthKeys = Object.keys(JSONData);
 
-  const [showDiv, setShowDiv] = useState(false);
+  const [showDiv, setShowDiv] = useState(0);
 
   // const secondDepthValue = Object.values(firstDepthValue[2]);
 
-  const executeMe = () => setShowDiv(!showDiv);
+  const executeMe = (key) =>{ 
+    setShowDiv(key);
+    console.log('executed');
+    console.log(key);
+    const firstDepthValue = Object.values(JSONData);
+    const secondDepthKeys = Object.keys(firstDepthValue[key]);
+    console.log(secondDepthKeys);
 
+    return secondDepthKeys.map((button, key) => {
+      return showDiv && (
+        <div
+          key={key}
+          className='w3-sidebar w3-bar-block w3-black w3-card'
+          style={{ width: '130px' }}>
+          <span className='w3-bar-item w3-button tablink' >
+            {button}
+          </span>
+        </div>
+      );
+    });
+  }
   const renderFirstDepthButtons = (firstDepthKeys) => {
-    while (firstDepthKeys.length) {
-      return firstDepthKeys.map((button, counter) => {
+    // while (firstDepthKeys.length) {
+      return firstDepthKeys.map((button, key) => {
         return (
-          <span key={counter}>
+          <span key={key}>
             <button
               className='w3-bar-item w3-button w3-blue '
-              onClick={() => executeMe}>
+              onClick={() => executeMe(key)}>
               {button}
             </button>
-            {showDiv && (
-              <SecondDepthButtons counter={counter} JSONData={JSONData} />
-            )}
+            {/* {showDiv && (
+              SecondDepthButtons(key)
+            )} */}
           </span>
         );
       });
-    }
+    // }
   };
+
+  const SecondDepthButtons = (key) => {
+    console.log('function executed');
+  console.log(key);
+    // setShowDiv(!showDiv);
+    const firstDepthValue = Object.values(JSONData);
+    const secondDepthKeys = Object.keys(firstDepthValue[key]);
+    // console.log(secondDepthKeys);
+  
+    return secondDepthKeys.map((button, key) => {
+      return (
+        <div
+          key={key}
+          className='w3-sidebar w3-bar-block w3-black w3-card'
+          style={{ width: '130px' }}>
+          <span className='w3-bar-item w3-button tablink' >
+            {button}
+          </span>
+        </div>
+      );
+    });
+  };
+  
 
   return renderFirstDepthButtons(firstDepthKeys);
 };
